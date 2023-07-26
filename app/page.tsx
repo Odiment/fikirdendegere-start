6fc62917f2128147e3e11ff4b897b599fac77a42
webintum <webintum@gmail.com>
2023-07-26 13:15:00 +0300

    header düzenlemeleri

---
+++ app/page.tsx
@@ -0,0 +1,72 @@
+import { cookies } from "next/headers"
+import Link from "next/link"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+import LogoutButton from "../components/LogoutButton"
+
+export default async function IndexPage() {
+/*   const supabase = createServerComponentClient({ cookies })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser() */
+
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+{/*       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
+          <div />
+          <div>
+            {user ? (
+              <div className="flex items-center gap-4">
+                Hey, {user.email}!
+                <LogoutButton />
+              </div>
+            ) : (
+              <Link
+                href="/login"
+                rel="noreferrer"
+                className={buttonVariants({ variant: "outline" })}
+                 className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+                
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav> */}
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Beautifully designed components <br className="hidden sm:inline" />
+          built with Radix UI and Tailwind CSS.
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Accessible and customizable components that you can copy and paste
+          into your apps. Free. Open Source. And Next.js 13 Ready.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
+  )
+}
